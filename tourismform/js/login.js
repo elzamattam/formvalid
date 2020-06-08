d060787c5e01672bae914ee201cb39a55b982818
elzamattam <62979150+elzamattam@users.noreply.github.com>
2020-06-08 23:59:25 +0530

    Add files via upload

---
+++ tourismform/js/login.js
@@ -0,0 +1,13 @@
+
+function logval()
+var email=document.getElementById("email").value
+var pwd=document.getElementById("password").value
+{
+    if (email==null || email==""){  
+        alert("Email can't be blank");  
+        return false;  
+      }else if(pwd.length<6){  
+        alert("Password must be at least 6 characters long.");  
+        return false;  
+        }  
+      }  
